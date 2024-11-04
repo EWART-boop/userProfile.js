b03166b6184f9b72f33844f2ed27999a3bf6c368
EWART-boop <johnewartdelacruz@gmail.com>
2024-11-04 17:48:20 +0800

    userProfile.js.

---
+++ app.js
@@ -0,0 +1,11 @@
+
+let fullName = "Sinio Goat"; 
+let age = 55;
+let favoriteNumber = 14; 
+let favoriteColor = "Black"; 
+
+
+console.log("Full Name: " + fullName);
+console.log("Age: " + age);
+console.log("Favorite Number: " + favoriteNumber);
+console.log("Favorite Color: " + favoriteColor);
